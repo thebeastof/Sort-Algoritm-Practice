c07498445ad224bc6d480eeab57432ef795aa07b
Hareheart <89731534+Hareheart@users.noreply.github.com>
2023-01-25 22:16:18 +0100

    Finished selection code and added descriptions

--- Algorithms/algorithms.py
+++ Algorithms/algorithms.py
@@ -11,6 +11,7 @@ def main(algorithm, string_list):
         print(unsorted_list)
     elif algorithm == "selection":
         selection_sort(unsorted_list)
+        print(unsorted_list)
     elif algorithm == "insertion":
         insertion_sort(unsorted_list)
     else:
@@ -24,9 +25,14 @@ def validate_list(l):
         return None
 
 def bubble_sort(unsorted_list):
+    # Iterates through array and compares element it is at with element to the right
+    # If element on left is greater than on right, the two switch
+    # Process is repeated until array is sorted
+
     length = len(unsorted_list) - 1
     sorted = False
-    while sorted != True:
+
+    while sorted == False:
         sorted = True
         for num in range(0, length):
             if unsorted_list[num] > unsorted_list[num + 1]:
@@ -35,9 +41,24 @@ def bubble_sort(unsorted_list):
     
     return unsorted_list
 
-def selection_sort(l):
-    # your code here
-    print(l)
+def selection_sort(unsorted_list):
+    # Iterates through array and compares every element to minimum value (first element)
+    # If an element is less than minimum, minimum = that element
+    # When array has been iterated through, final minimum value is replaced with first element
+    # Process is repeated until array is sorted
+
+    minimum = unsorted_list[0]
+    length = len(unsorted_list) - 1
+
+    for elem in range(0, length):
+        minimum = elem
+        for num in range(elem + 1, len(unsorted_list)):
+            if unsorted_list[num] < unsorted_list[minimum]:
+                minimum = num
+        if minimum != elem:
+            unsorted_list[minimum], unsorted_list[elem] = unsorted_list[elem], unsorted_list[minimum]
+
+    return unsorted_list
 
 def insertion_sort(l):
     # your code here
